085e42f81359c1c0df787250ecff1d014021bf13
JosephO <joe.olmez@gmail.com>
2022-12-06 12:11:36 -0500

    first commit

---
+++ src/main/java/com/olmez/mya/MyaApplication.java
@@ -0,0 +1,13 @@
+package com.olmez.mya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaApplication.class, args);
+	}
+
+}
